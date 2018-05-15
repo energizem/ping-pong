17b195c85292ab17038f34fe1a69a6bd83672e7b
borislavmiletic <Borislav.miletich@gmail.com>
2018-05-15 22:03:59 +0200

    PhysicsUpdate
    Fixed ball jumping our boundires

---
+++ ping pong/Assets/BallSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeed : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (rb2d.velocity.magnitude > maxSpeed)
+        {
+            rb2d.velocity = rb2d.velocity.normalized * maxSpeed;
+        }
+    }
+}
--- ping pong/Assets/PlayerMovement.cs
+++ ping pong/Assets/PlayerMovement.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float speed = 100;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb2d;
 
-    public void Update()
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        //if (Input.GetKey(KeyCode.LeftControl)){ 
+
         Vector3 tempVect = new Vector3(h, v, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
-        rb.MovePosition(rb.transform.position + tempVect);
+
+        rb2d.MovePosition(rb2d.transform.position + tempVect);
+        
+        //}
     }
 }
