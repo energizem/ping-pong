43a2bff6ef8f0cedcdf2129647e9b50a04ab13b0
borislavmiletic <Borislav.miletich@gmail.com>
2018-05-17 14:09:21 +0200

    d

--- ping pong/Assets/PlayerMovement.cs
+++ ping pong/Assets/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+
+    public KeyCode[] p1 = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
     public float speed = 100;
-    private Rigidbody2D rb2d;
+    public Rigidbody2D rb2d,rb2;
 
     void Start()
     {
@@ -18,13 +20,14 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        //if (Input.GetKey(KeyCode.LeftControl)){ 
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        { 
 
         Vector3 tempVect = new Vector3(h, v, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
 
         rb2d.MovePosition(rb2d.transform.position + tempVect);
         
-        //}
+        }
     }
 }
